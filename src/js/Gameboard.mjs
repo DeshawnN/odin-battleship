faafe25dd3fee52caf566e2e6b82d0c0920fc575
Deshawn Nelson <Deshawn.a.nelson@gmail.com>
2022-02-28 22:18:57 -0500

    feat: finalize the gameboard module logic

--- src/js/Gameboard.mjs
+++ src/js/Gameboard.mjs
@@ -1,25 +1,61 @@
 export default function Gameboard() {
+
+    function moveValidator(moveArr) {
+        return moveArr.every(function(move) {
+            return move === undefined;
+        });
+    };
     return {
         board: new Array(10).fill().map(() => new Array(10)),
         ships: [],
         missed: new Array(10).fill().map(() => new Array(10)),
         placeShip: function(ship, yCoord, xCoord, isVertical) {
+            const length = ship.getLength();
+
+            if (this.board[yCoord][xCoord]) return false;
+
             if (isVertical) {
+                if (yCoord + length > 10) return false;
+
+                const coords = [];
+                for (let i = 0; i < ship.getLength(); i++) {
+                    coords.push(this.board[yCoord + i][xCoord]);
+                }
+
+                const isvalidMove = moveValidator(coords);
+
+                if (isvalidMove === false) return false;
+
                 for (let i = 0; i < ship.getLength(); i++) {
                     this.board[yCoord + i][xCoord] = { ship, position: i};
                 }
             } else {
+                if (xCoord + length > 10) return false;
+
+                const coords = [];
+                for (let i = 0; i < ship.getLength(); i++) {
+                    coords.push(this.board[yCoord][xCoord + i]);
+                }
+
+                const isvalidMove = moveValidator(coords);
+
+                if (isvalidMove === false) return false;
+
                 for (let i = 0; i < ship.getLength(); i++) {
                     this.board[yCoord][xCoord + i] = { ship, position: i};
                 }
             }
             this.ships.push(ship);
+
+            return true;
         },
-        recieveAttack: function(yCoord, xCoord) {
+        receiveAttack: function(yCoord, xCoord) {
             if (!this.board[yCoord][xCoord]) {
                 this.board[yCoord][xCoord] = { missed: true };
                 return;
             }
+
+            if (this.board[yCoord][xCoord].missed) return;
 
             const position = this.board[yCoord][xCoord].position;
             this.board[yCoord][xCoord].ship.hit(position);
@@ -28,7 +64,10 @@ export default function Gameboard() {
             return this.board[yCoord][xCoord];
         },
         allShipsSunk: function() {
-            return this.ships.every(ship => ship.sunk);
+            const allShipsSunk = this.ships.every(function(ship) {
+                return ship.sunk === true;
+            });
+            return allShipsSunk;
         }
     }
 }

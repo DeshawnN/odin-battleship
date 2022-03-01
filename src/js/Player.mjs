2da820709d83d6fec7ffd83ce5fc8aaa4cf7e5cb
Deshawn Nelson <Deshawn.a.nelson@gmail.com>
2022-02-28 22:19:14 -0500

    feat: finalize the Player module logic

--- src/js/Player.mjs
+++ src/js/Player.mjs
@@ -1,14 +1,14 @@
 export default function Player() {
     return {
         sendAttack: function(gameboard, yCoord, xCoord) {
-            gameboard.recieveAttack(yCoord, xCoord);
+            gameboard.receiveAttack(yCoord, xCoord);
         },
         generateRandomCoords: function(gameboard) {
             const length = gameboard.board.length;
             const y = Math.floor(Math.random() * length);
             const x = Math.floor(Math.random() * length);
 
-            return { x, y }
+            return { y, x }
         }
     }
 }
